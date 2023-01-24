0eab2fbedd204d0fb264553a55486d29c12fe4ab
Joao Paulo <oliveirajpufrj@gmail.com>
2023-01-24 02:39:44 -0300

    feat: get all tweets w/ set page & set number of tweets per page

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -3,12 +3,14 @@ package com.tweteroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,18 +23,26 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/tweets")
 public class TweetController {
-    
+
     @Autowired
     private TweetService service;
 
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Tweet postTweet(@RequestBody @Valid TweetDTO data){
+    public Tweet postTweet(@RequestBody @Valid TweetDTO data) {
         return service.postTweet(data);
     }
 
     @GetMapping("/{username}")
-    public List<Tweet> getUserTweets(@PathVariable String username){
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public List<Tweet> getUserTweets(@PathVariable String username) {
         return service.getUserTweets(username);
+    }
+
+    @GetMapping()
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Page<Tweet> list(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "5") int size) {
+        return service.getTweets(page, size);
     }
 }
--- src/main/java/com/tweteroo/api/service/TweetService.java
+++ src/main/java/com/tweteroo/api/service/TweetService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.tweteroo.api.dto.TweetDTO;
@@ -14,20 +17,27 @@ import com.tweteroo.api.repository.TweetRepository;
 
 @Service
 public class TweetService {
-    
+
     @Autowired
     private TweetRepository repository;
 
     @Autowired
     private AuthRepository userRepository;
 
-    public Tweet postTweet(TweetDTO data){
-        Optional<Person> user =  userRepository.findByUsername(data.username());
+    public Tweet postTweet(TweetDTO data) {
+        Optional<Person> user = userRepository.findByUsername(data.username());
         Tweet userTweet = new Tweet(data, user.get().getAvatar());
         return repository.save(userTweet);
     }
 
-    public List<Tweet> getUserTweets(String username){
+    public List<Tweet> getUserTweets(String username) {
         return repository.findByUsername(username);
+    }
+
+    public Page<Tweet> getTweets(int page, int size) {
+        Sort sort = Sort.by("id").descending();
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+
+        return repository.findAll(pageRequest);
     }
 }
